e5d46529fb68e669e8cbd932013fe5e0d4817d73
Michael Südkamp <ms2702@online.de>
2024-07-27 15:10:36 +0200

    Test mit weiterem Path

--- node-express-1/handler.js
+++ node-express-1/handler.js
@@ -14,6 +14,12 @@ app.get("/hello", (req, res, next) => {
   });
 });
 
+app.get("/hello2", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path hello2!",
+  });
+});
+
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
